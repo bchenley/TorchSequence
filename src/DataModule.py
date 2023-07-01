0cb0dfadbbaef49302f6eaca480a85752be49093
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-01 18:13:57 -0400

    Update DataModule.py

--- src/DataModule.py
+++ src/DataModule.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import pandas as pd
 
-from src import SequenceDataloader
+from src import SequenceDataset
 
 class DataModule(pl.LightningDataModule):
   def __init__(self,
