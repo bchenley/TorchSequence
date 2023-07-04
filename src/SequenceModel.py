7afdcd1b8f58f7a88f223d11b832d13218dd7ed1
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-04 15:39:01 -0400

    Update SequenceModel.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/SequenceModel.py
+++ src/SequenceModel.py
@@ -95,9 +95,12 @@ class SequenceModel(torch.nn.Module):
       value = locals_copy[arg]
 
       if isinstance(value, list) and any(x in arg for x in ['seq_type', 'input_size', 'base_', 'decoder_', 'hidden_', 'attn_']):  
-        if len(value) == 1:          
+        if len(value) == 1:
+          if arg == 'base_transformer_feedforward_activation':
+            print(base_transformer_feedforward_activation)
           exec(f"{arg} = {value} * num_inputs") # setattr(self, arg, value * num_inputs)
-          
+          if arg == 'base_transformer_feedforward_activation':
+            print(base_transformer_feedforward_activation)
       elif isinstance(value, list) and any(x in arg for x in ['output_size', 'output_']):        
         if len(value) == 1:          
           exec(f"{arg} = {value} * num_outputs") # setattr(self, arg, value * num_outputs)
