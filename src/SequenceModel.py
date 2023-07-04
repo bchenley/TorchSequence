10a0a384368d2c0fbb803b586002f58de8191d0d
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-04 16:17:51 -0400

    Update SequenceModel.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/SequenceModel.py
+++ src/SequenceModel.py
@@ -95,10 +95,10 @@ class SequenceModel(torch.nn.Module):
       value = locals_[arg]
       if isinstance(value, list) and any(x in arg for x in ['seq_type', 'input_size', 'base_', 'decoder_', 'hidden_', 'attn_']):  
         if len(value) == 1:          
-          setattr(self, arg, value * num_inputs)   
+          value = value * num_inputs # setattr(self, arg, value * num_inputs)   
       elif isinstance(value, list) and any(x in arg for x in ['output_size', 'output_']):        
         if len(value) == 1:          
-          setattr(self, arg, value * num_outputs)
+          value = value * num_outputs # setattr(self, arg, value * num_outputs)
     
     seq_base, hidden_layer = torch.nn.ModuleList([]), torch.nn.ModuleList([])
     for i in range(num_inputs):
