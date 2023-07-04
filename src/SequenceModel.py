c82dad8772eeb81739b88fc77ba1764d06eca3dd
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-04 14:42:23 -0400

    Update SequenceModel.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/SequenceModel.py
+++ src/SequenceModel.py
@@ -85,22 +85,22 @@ class SequenceModel(torch.nn.Module):
                output_dropout_p = [0.],
                #
                device = 'cpu', dtype = torch.float32):
-
+    
     super(SequenceModel, self).__init__()
 
     self.to(device = device, dtype = dtype)
-
+    
     locals_copy = locals().copy() # copy the local variables
     for arg in locals_copy:
       value = locals_copy[arg]
-
+      
       if isinstance(value, list) and any(x in arg for x in ['seq_type', 'input_size', 'base_', 'decoder_', 'hidden_', 'attn_']):  
-        if len(value) == 1:          
+        if len(value) == 1:
           setattr(self, arg, value * num_inputs)
       elif isinstance(value, list) and any(x in arg for x in ['output_size', 'output_']):
         if len(value) == 1:
           setattr(self, arg, value * num_outputs)
-
+      
     seq_base, hidden_layer = torch.nn.ModuleList([]), torch.nn.ModuleList([])
     for i in range(num_inputs):
       # input-associated sequence layer
