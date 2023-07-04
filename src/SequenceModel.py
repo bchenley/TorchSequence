30f67e057f1ab6e0158ea27dd938fee8e49fca91
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-04 13:38:20 -0400

    Update SequenceModel.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/SequenceModel.py
+++ src/SequenceModel.py
@@ -93,8 +93,8 @@ class SequenceModel(torch.nn.Module):
     locals_copy = locals().copy() # copy the local variables
     for arg in locals_copy:
       value = locals_copy[arg]
-      print(value)
-      if isinstance(value, list) and any(x in arg for x in ['seq_type', 'input_size', 'base_', 'decoder_', 'hidden_', 'attn_']):
+      
+      if isinstance(value, list) and any(x in value for x in ['seq_type', 'input_size', 'base_', 'decoder_', 'hidden_', 'attn_']):
         if value == 'base_transformer_feedforward_activation':
           print(base_transformer_feedforward_activation, len(base_transformer_feedforward_activation))
         if len(value) == 1:
@@ -102,7 +102,7 @@ class SequenceModel(torch.nn.Module):
         if value == 'base_transformer_feedforward_activation':
           print(base_transformer_feedforward_activation, len(base_transformer_feedforward_activation))
           
-      elif isinstance(value, list) and any(x in arg for x in ['output_size', 'output_']):
+      elif isinstance(value, list) and any(x in value for x in ['output_size', 'output_']):
         if len(value) == 1:
           setattr(self, arg, value * num_outputs)
 
