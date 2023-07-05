254dc336e406d04d305fb46705608da08db52a0b
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-05 03:06:40 -0400

    Update __init__.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/__init__.py
+++ src/__init__.py
@@ -2,48 +2,40 @@ print("Initializing TorchTimeSeries package...")
 
 import importlib, pkgutil
 
-# __all__ = ['FeatureTransform',
-#            'Loss', 
-#            'fft', 
-#            'periodogram', 
-#            'moving_average', 
-#            'butter', 
-#            'fill', 
-#            'Interpolator', 
-#            'remove_outliers', 
-#            'BaselineModel', 
-#            'Polynomial', 
-#            'LRU', 
-#            'HiddenLayer', 
-#            'ModulationLayer',
-#            'LegendreModulator',
-#            'ChebychevModulator', 
-#            'FourierModulator', 
-#            'SigmoidModulator', 
-#            'Attention', 
-#            'TransformerEncoderLayer',
-#            'TransformerDecoderLayer', 
-#            'SequenceModelBase', 
-#            'SequenceModel', 
-#            'Seq2SeqModel', 
-#            'Embedding', 
-#            'PositionalEncoding', 
-#            'SequenceDataset',                        
-#            'SequenceDataloader',
-#            'DataModule',
-#            'SequenceModule']
+__all__ = ['FeatureTransform',
+           'Loss', 
+           'fft', 
+           'periodogram', 
+           'moving_average', 
+           'butter', 
+           'fill', 
+           'Interpolator', 
+           'remove_outliers', 
+           'BaselineModel', 
+           'Polynomial', 
+           'LRU', 
+           'HiddenLayer', 
+           'ModulationLayer',
+           'LegendreModulator',
+           'ChebychevModulator', 
+           'FourierModulator', 
+           'SigmoidModulator', 
+           'Attention', 
+           'TransformerEncoderLayer',
+           'TransformerDecoderLayer', 
+           'SequenceModelBase', 
+           'SequenceModel', 
+           'Seq2SeqModel', 
+           'Embedding', 
+           'PositionalEncoding', 
+           'SequenceDataset',                        
+           'SequenceDataloader',
+           'DataModule',
+           'SequenceModule']
 
-# for module_name in __all__:
-#     module = importlib.import_module(f'.{module_name}', __name__)
-#     globals()[module_name] = getattr(module, module_name)
+for module_name in __all__:
+    module = importlib.import_module(f'.{module_name}', __name__)
+    globals()[module_name] = getattr(module, module_name)
 
-package = 'src'
-__all__ = []
-
-for module_info in pkgutil.iter_modules([package]):
-  module_name = module_info.name
-  __all__.append(module_name)             
-  module = importlib.import_module(f'.{module_name}')
-  globals()[module_name] = getattr(module, module_name)
            
 print("Done")
