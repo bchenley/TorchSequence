e3596a52f6859e9f5a301dda1daf0265f741ac00
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-05 00:40:56 -0400

    Update HiddenLayer.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- src/HiddenLayer.py
+++ src/HiddenLayer.py
@@ -32,7 +32,8 @@ class HiddenLayer(torch.nn.Module):
     locals_ = locals().copy()
 
     for arg in locals_:
-      setattr(self, arg, locals_[arg])
+      if arg != 'self':
+        setattr(self, arg, locals_[arg])
       
     # self.to(device = self.device, dtype = self.dtype)
   
