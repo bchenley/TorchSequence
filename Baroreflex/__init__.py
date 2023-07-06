94697b11e63c2a966d56c29d0bb6284e746d6772
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-06 13:48:53 -0400

    Create __init__.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

---
+++ Baroreflex/__init__.py
@@ -0,0 +1,11 @@
+print("Initializing Beat2BeatAnalysis...")
+
+import importlib
+
+__all__ = ['Beat2BeatAnalyzer']
+
+for module_name in __all__:
+    module = importlib.import_module(f'.{module_name}', __name__)
+    globals()[module_name] = getattr(module, module_name)
+
+print("Done")
