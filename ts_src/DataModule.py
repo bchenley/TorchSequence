fc536eca31c087f68cd27bda9dee76e5f6cd68b2
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-18 15:08:08 -0400

    Update DataModule.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/DataModule.py
+++ ts_src/DataModule.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import pandas as pd
 
-from ts_src import SequenceDataloader.SequenceDataloader as SequenceDataloader
+from ts_src.SequenceDataloader import SequenceDataloader
 
 class DataModule(pl.LightningDataModule):
   def __init__(self,
