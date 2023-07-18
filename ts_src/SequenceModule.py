bfc0f3b196b527c2a0c1ea83b7e46e40f27918d2
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-18 14:52:48 -0400

    Update SequenceModule.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceModule.py
+++ ts_src/SequenceModule.py
@@ -6,7 +6,7 @@ import time
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
 
-from ts_src import Loss
+from ts_src import Loss as Loss
 
 class SequenceModule(pl.LightningModule):
   def __init__(self,
