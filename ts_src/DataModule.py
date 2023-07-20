095bd96a36012b6deee83361edd00ddb9cc0d251
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-20 12:34:43 -0400

    Update DataModule.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/DataModule.py
+++ ts_src/DataModule.py
@@ -54,7 +54,7 @@ class DataModule(pl.LightningDataModule):
       for arg in locals_:
         if arg != 'self':
           if arg == 'data':
-            setattr(self, arg, locals_[arg].copy() if arg == 'data' else lcoals_[arg])  
+            setattr(self, arg, locals_[arg].copy() if arg == 'data' else locals_[arg])  
           
       self.total_input_len = np.max(input_len).item()
       self.total_output_len = np.max(output_len).item()
