0097ff3e06cb3753b39f50ad9b7ee7282e34680a
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-20 13:02:47 -0400

    Update SequenceDataloader.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceDataloader.py
+++ ts_src/SequenceDataloader.py
@@ -38,8 +38,7 @@ class SequenceDataloader(torch.utils.data.Dataset):
     locals_ = locals().copy()                   
     for arg in locals_:
       if arg != 'self':
-        if arg == 'data':
-          setattr(self, arg, locals_[arg].copy() if arg == 'data' else locals_[arg])
+        setattr(self, arg, locals_[arg].copy() if arg == 'data' else locals_[arg])
 
     self.dl = self.get_dataloader
 
