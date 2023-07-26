41c229a8c01dedea4f6292a58a01de6169577353
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-26 16:47:32 -0400

    Update SequenceModule.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceModule.py
+++ ts_src/SequenceModule.py
@@ -373,7 +373,7 @@ class SequenceModule(pl.LightningModule):
     for param in history:
       ax_i += 1
       ax = fig.add_subplot(num_params, 1, ax_i)
-      ax.plot(train_history[x_label], train_history[param], label = 'Train')
+      ax.plot(train_history[x_label].cpu(), train_history[param].cpu(), label = 'Train')
       if (self.val_history is not None) & (param in self.val_history) & (x_label == 'epochs'):
         N = np.min([self.val_history[x_label].shape[0], self.val_history[param].shape[0]])
 
@@ -383,7 +383,7 @@ class SequenceModule(pl.LightningModule):
           if self.metric_fn.name in param:
             metric = self.val_history[param][:N]
 
-        ax.plot(self.val_history[x_label][:N], metric, label = 'Val')
+        ax.plot(self.val_history[x_label][:N].cpu(), metric.cpu(), label = 'Val')
       ax.set_title(param)
       ax.set_xlabel(x_label)
       ax.set_ylabel(param)
