d465f1c02e54fe3a4b395139b24c5e0f1516d565
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-07-27 14:29:50 -0400

    Update Loss.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/Loss.py
+++ ts_src/Loss.py
@@ -1,50 +1,50 @@
-class Loss():
+class Criterion():
   '''
-  A class for computing loss functions.
+  A class for computing criterion functions.
   '''
 
   def __init__(self, name='mse', dims=0):
     '''
-    Initializes the Loss instance.
+    Initializes the Criterion instance.
 
     Args:
-        name (str): The name of the loss function. Options are 'mae', 'mse', 'mase', 'rmse', 'nmse', 'mape', 'fb'.
-        dims (int): The dimension along which the loss is computed.
+        name (str): The name of the criterion function. Options are 'mae', 'mse', 'mase', 'rmse', 'nmse', 'mape', 'fb'.
+        dims (int): The dimension along which the criterion is computed.
     '''
     self.name = name
     self.dims = dims
 
   def __call__(self, y_pred, y_true):
     '''
-    Computes the loss based on the predicted and true values.
+    Computes the criterion based on the predicted and true values.
 
     Args:
         y_pred (torch.Tensor): The predicted values.
         y_true (torch.Tensor): The true values.
 
     Returns:
-        torch.Tensor: The computed loss value.
+        torch.Tensor: The computed criterion value.
     '''
     if self.name == 'mae':
         # Mean Absolute Error (L1 loss)
-        loss = (y_true - y_pred).abs().nanmean(dim=self.dims)
+        criterion = (y_true - y_pred).abs().nanmean(dim=self.dims)
     elif self.name == 'mse':
         # Mean Squared Error
-        loss = (y_true - y_pred).pow(2).nanmean(dim=self.dims)
+        criterion = (y_true - y_pred).pow(2).nanmean(dim=self.dims)
     elif self.name == 'mase':
         # Mean Absolute Scaled Error
-        loss = (y_true - y_pred).abs().nanmean(dim=self.dims) / (y_true.diff(n=1, dim=self.dims).abs().nanmean(dim=self.dims))
+        criterion = (y_true - y_pred).abs().nanmean(dim=self.dims) / (y_true.diff(n=1, dim=self.dims).abs().nanmean(dim=self.dims))
     elif self.name == 'rmse':
         # Root Mean Squared Error
-        loss = (y_true - y_pred).pow(2).nanmean(dim=self.dims).sqrt()
+        criterion = (y_true - y_pred).pow(2).nanmean(dim=self.dims).sqrt()
     elif self.name == 'nmse':
         # Normalized Mean Squared Error
-        loss = (y_true - y_pred).pow(2).nanmean(dim=self.dims) / y_true.pow(2).nanmean(dim=self.dims)
+        criterion = (y_true - y_pred).pow(2).nanmean(dim=self.dims) / y_true.pow(2).nanmean(dim=self.dims)
     elif self.name == 'mape':
         # Mean Absolute Percentage Error
-        loss = (((y_true - y_pred) / y_true).abs() * 100).nanmean(dim=self.dims)
+        criterion = (((y_true - y_pred) / y_true).abs() * 100).nanmean(dim=self.dims)
     elif self.name == 'fb':
         # Fractional Bias
-        loss = (y_pred.nansum(dim=self.dims) - y_true.nansum(dim=self.dims)) / y_true.nansum(dim=self.dims)
+        criterion = (y_pred.nansum(dim=self.dims) - y_true.nansum(dim=self.dims)) / y_true.nansum(dim=self.dims)
 
-    return loss
+    return criterion
