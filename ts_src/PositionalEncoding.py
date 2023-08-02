60123c12ca2431973119d5f4cce737541805d45b
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-02 15:11:55 -0400

    Update PositionalEncoding.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/PositionalEncoding.py
+++ ts_src/PositionalEncoding.py
@@ -6,7 +6,7 @@ class PositionalEncoding(torch.nn.Module):
 
   Args:
     dim (int): Dimensionality of the input.
-    seq_len (int): Length of the input sequence.
+    input_len (int): Length of the input sequence.
     encoding_type (str, optional): Type of positional encoding to use. Supported types: 'absolute', 'relative'.
                                     Defaults to 'absolute'.
     device (str, optional): Device on which the positional encoding layer is allocated. Defaults to 'cpu'.
@@ -14,7 +14,7 @@ class PositionalEncoding(torch.nn.Module):
   '''
 
   def __init__(self,
-                dim, seq_len, encoding_type='absolute',
+                dim, input_len, encoding_type='absolute',
                 device='cpu', dtype=torch.float32):
       super(PositionalEncoding, self).__init__()
 
@@ -31,13 +31,13 @@ class PositionalEncoding(torch.nn.Module):
       Generates the positional encoding based on the encoding type.
 
       Returns:
-        torch.Tensor: Positional encoding tensor of shape (seq_len, dim).
+        torch.Tensor: Positional encoding tensor of shape (input_len, dim).
       '''
 
-      position = torch.arange(self.seq_len).unsqueeze(1).to(device=self.device, dtype=self.dtype)
+      position = torch.arange(self.input_len).unsqueeze(1).to(device=self.device, dtype=self.dtype)
 
       if self.encoding_type == 'absolute':
-          positional_encoding = torch.zeros((self.seq_len, self.dim)).to(device=self.device, dtype=self.dtype)
+          positional_encoding = torch.zeros((self.input_len, self.dim)).to(device=self.device, dtype=self.dtype)
 
           scaler = torch.exp(torch.arange(0, self.dim, 2) * -(torch.math.log(10000.0) / self.dim)).to(
               device=self.device, dtype=self.dtype)
@@ -47,7 +47,7 @@ class PositionalEncoding(torch.nn.Module):
 
       elif self.encoding_type == 'relative':
           positional_encoding = (position.repeat(1, self.dim) +
-                                  torch.arange(self.dim).reshape(1, -1).to(device=self.device, dtype=self.dtype)) / self.seq_len
+                                  torch.arange(self.dim).reshape(1, -1).to(device=self.device, dtype=self.dtype)) / self.input_len
 
           positional_encoding = positional_encoding / positional_encoding.max()
 
@@ -58,10 +58,10 @@ class PositionalEncoding(torch.nn.Module):
       Forward pass of the positional encoding layer.
 
       Args:
-        input (torch.Tensor): Input tensor of shape (batch_size, seq_len, dim).
+        input (torch.Tensor): Input tensor of shape (batch_size, input_len, dim).
 
       Returns:
-        torch.Tensor: Input tensor with added positional encoding of shape (batch_size, seq_len, dim).
+        torch.Tensor: Input tensor with added positional encoding of shape (batch_size, input_len, dim).
       '''
 
       return input + self.positional_encoding[:input.shape[1], :]
