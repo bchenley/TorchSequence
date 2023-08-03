25414637c73cb59c38ff33825d2fb4b849a276cd
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-03 13:52:36 -0400

    Update Seq2SeqModel.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/Seq2SeqModel.py
+++ ts_src/Seq2SeqModel.py
@@ -22,12 +22,12 @@ class Seq2SeqModel(torch.nn.Module):
   '''
     
   def __init__(self,
-              encoder, decoder,
-              learn_decoder_input=False, learn_decoder_hiddens=False,
-              enc2dec_bias=True, enc2dec_hiddens_bias=True,
-              enc2dec_dropout_p=0., enc2dec_hiddens_dropout_p=0.,
-              enc_out_as_dec_in = False,
-              device='cpu', dtype=torch.float32):
+               encoder, decoder,
+               learn_decoder_input = False, learn_decoder_hiddens = False,
+               enc2dec_bias = True, enc2dec_hiddens_bias = True,
+               enc2dec_dropout_p = 0., enc2dec_hiddens_dropout_p = 0.,
+               enc_out_as_dec_in = False,
+               device = 'cpu', dtype = torch.float32):
 
     super(Seq2SeqModel, self).__init__()
 
@@ -42,8 +42,8 @@ class Seq2SeqModel(torch.nn.Module):
     self.base_type = self.encoder.base_type
                 
     self.enc2dec_input_block = None
-    if self.learn_decoder_input:
-      
+
+    if self.learn_decoder_input:      
       self.enc2dec_input_block = HiddenLayer(in_features = sum(self.encoder.input_size),
                                              out_features = sum(self.decoder.input_size),
                                              bias = self.enc2dec_bias,
@@ -52,21 +52,26 @@ class Seq2SeqModel(torch.nn.Module):
                                              device = self.device,
                                              dtype = self.dtype)
       
-    self.enc2dec_hiddens_block = None
-    
+    self.enc2dec_hiddens_block = None    
     if self.learn_decoder_hiddens:
       if any(type_ in ['gru', 'lstm', 'lru'] for type_ in self.encoder.base_type):
         total_encoder_hidden_size = 0
         for i in range(self.encoder.num_inputs):
-          if self.encoder.base_type[i] in ['gru', 'lstm', 'lru']:
-            total_encoder_hidden_size += (1 + int(self.encoder.base_type[i] == 'lstm')) * (1 + int(self.encoder.base_rnn_bidirectional[i])) * self.encoder.base_num_layers[i] * self.encoder.base_hidden_size[i]      
+          if self.encoder.base_type[i] in ['lstm','gru']:
+            total_encoder_hidden_size += (1 + int(self.encoder.base_rnn_bidirectional[i])) * self.encoder.base_hidden_size[i]      
+          elif self.encoder.base_type[i] == 'lru':
+            total_encoder_hidden_size += self.encoder.base_num_filterbanks[i] * self.encoder.base_hidden_size[i]      
+          
       else:
         total_encoder_hidden_size = sum(self.encoder.output_size)
 
       self.decoder_hidden_size = []
-      for i in range(self.num_inputs):
-        self.decoder_hidden_size.append(0)        
-        self.decoder_hidden_size[i] = (1+int(self.decoder.base_type[i] == 'lstm')) * (1 + int(self.decoder.base_rnn_bidirectional[i])) * self.decoder.base_num_layers[i] * self.decoder.base_hidden_size[i]
+      for i in range(self.decoder.num_inputs):
+        self.decoder_hidden_size.append(0)
+        if self.decoder.base_type[i] in ['lstm','gru']:
+          self.decoder_hidden_size[i] = (1 + int(self.decoder.base_rnn_bidirectional[i])) * self.decoder.base_num_layers[i] * self.decoder.base_hidden_size[i]
+        elif self.decoder.base_type[i] == 'lru':
+          self.decoder_hidden_size[i] = self.decoder.base_num_filterbanks[i] * self.decoder.base_hidden_size[i]
       
       self.enc2dec_hiddens_block = HiddenLayer(in_features = total_encoder_hidden_size,
                                                out_features = sum(self.decoder_hidden_size),
@@ -78,15 +83,15 @@ class Seq2SeqModel(torch.nn.Module):
   
   def forward(self,
               input,
-              steps=None,
-              hiddens=None,
-              input_mask=None, output_mask=None,
-              output_input_idx=[], input_output_idx=[],
-              encoder_output=None,
-              target=None,
-              input_window_idx=None,
-              output_window_idx=None):
-
+              steps = None,
+              hiddens = None,
+              input_mask = None, output_mask = None,
+              output_input_idx = [], input_output_idx = [],
+              encoder_output = None,
+              target = None,
+              input_window_idx = None,
+              output_window_idx = None):
+    
     '''
     Forward pass of the Seq2SeqModel.
 
@@ -106,7 +111,7 @@ class Seq2SeqModel(torch.nn.Module):
         torch.Tensor: Decoder output tensor of shape (num_samples, output_len, output_size).
         list: List of hidden states after the encoder.
     '''
-
+    
     num_samples, input_len, input_size = input.shape
     
     encoder_steps = steps[:, :input_len] if steps is not None else None
@@ -120,15 +125,18 @@ class Seq2SeqModel(torch.nn.Module):
     hiddens = encoder_hiddens
     
     decoder_hiddens = [None for _ in range(self.decoder.num_inputs)]
-
+    
     if self.enc2dec_hiddens_block is not None:
-      # If the enc2dec_hiddens_block exists (decoder must contain rnn's)
-      if encoder_hiddens is not None:
-        # If there are rnn hiddens
+      if any(type_ in ['gru', 'lstm', 'lru'] for type_ in self.encoder.base_type):
         enc2dec_hiddens_input = []
         for i in range(self.encoder.num_inputs):
-
-          enc2dec_hiddens_input.append(torch.cat(encoder_hiddens[i], -1).reshape(num_samples, -1) if self.encoder.base_type[i] == 'lstm' else encoder_hiddens[i].reshape(num_samples, -1))
+          if self.encoder.base_type[i] == 'lstm':
+            enc2dec_hiddens_input.append(encoder_hiddens[i][0][-1:])
+          elif self.encoder.base_type[i] == 'gru':
+            enc2dec_hiddens_input.append(encoder_hiddens[i][0][-1:])
+          elif self.encoder.base_type[i] == 'lru':
+            enc2dec_hiddens_input.append(encoder_hiddens[i][0].reshape(num_samples, -1))
+          
         enc2dec_hiddens_input = torch.cat(enc2dec_hiddens_input, -1)
       else:
         enc2dec_hiddens_input = encoder_output
@@ -137,17 +145,15 @@ class Seq2SeqModel(torch.nn.Module):
       
       j = 0
       for i in range(self.decoder.num_inputs):
-        if self.decoder.base_type[i] in ['gru', 'lstm', 'lru']:
+        if self.decoder.base_type[i] == 'lstm':
+          decoder_hiddens[i] = [enc2dec_hiddens_output[:, j:(j + self.decoder_hidden_size[i])].reshape(-1, num_samples, self.decoder.base_hidden_size[i])]
+          decoder_hiddens[i].append(torch.zeros_like(decoder_hiddens[i][0]))
+        if self.decoder.base_type[i] == 'gru':
+          decoder_hiddens[i] = enc2dec_hiddens_output[:, j:(j + self.decoder_hidden_size[i])].reshape(-1, num_samples, self.decoder.base_hidden_size[i])
+        if self.decoder.base_type[i] == 'lru':
+          decoder_hiddens[i] = enc2dec_hiddens_output[:, j:(j + self.decoder_hidden_size[i])].reshape(-1, num_samples, self.decoder.base_hidden_size[i])
 
-          split_size = (1 + int(self.decoder.base_rnn_bidirectional[i])) * self.decoder.base_num_layers[i] * self.decoder.base_hidden_size[i]
-          
-          decoder_hiddens_i = enc2dec_hiddens_output[:, j:(j + self.decoder_hidden_size[i])].split(split_size, 1)
-
-          if self.decoder.base_type[i] == 'lstm':
-            decoder_hiddens[i] = [h.reshape(-1, num_samples, self.decoder.base_hidden_size[i]) for h in decoder_hiddens_i]
-          else:
-            decoder_hiddens[i] = decoder_hiddens_i[0].reshape(-1, num_samples, self.decoder.base_hidden_size[i])          
-          j += self.decoder_hidden_size[i]
+        j += self.decoder_hidden_size[i]
           
     else:
       decoder_hiddens = None
