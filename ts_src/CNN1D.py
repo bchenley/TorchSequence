7d0c967c725667d654eda82d9aef59b8d7bed6a1
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-03 09:03:00 -0400

    Update CNN1D.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/CNN1D.py
+++ ts_src/CNN1D.py
@@ -9,7 +9,7 @@ class CNN1D(torch.nn.Module):
         in_channels (int): Number of input channels.
         out_channels (list): List of integers, specifying the number of output channels for each layer.
         kernel_size (list, optional): List of tuples specifying the kernel size for each layer. Default is [(1,)].
-        stride (list, optional): List of tuples specifying the stride for each layer. Default is [(1,)].
+        kernel_stride (list, optional): List of tuples specifying the stride for each layer. Default is [(1,)].
         padding (list, optional): List of tuples specifying the padding for each layer. Default is [(0,)].
         dilation (list, optional): List of tuples specifying the dilation for each layer. Default is [(1,)].
         groups (list, optional): List of integers specifying the number of groups for each layer. Default is [1].
@@ -17,12 +17,16 @@ class CNN1D(torch.nn.Module):
         pool_type (list, optional): List of strings specifying the pooling type for each layer. 
                                     Options: [None, 'max', 'avg']. Default is [None].
         pool_size (list, optional): List of tuples specifying the pooling size for each layer. Default is [2].
+        pool_stride (list, optional): List of tuples specifying the pooling stride for each layer. Default is [(1,)].
+        batch_norm (bool, optional): If True, applies batch normalization after each convolutional layer. Default is False.
+        batch_norm_learn (bool, optional): If True, allows the batch normalization layers to learn affine parameters. Default is False.
+        pad_front (bool, optional): If True, pads the front of the input tensor with zeros before applying the convolutional layers. Default is False.
         device (str, optional): Device on which the model parameters should be stored. Default is None.
         dtype (torch.dtype, optional): Data type for the model parameters. Default is None.
 
     Methods:
-        __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, 
-                  pool_type, pool_size, flatten, device, dtype):
+        __init__(self, in_channels, out_channels, kernel_size, kernel_stride, padding, dilation, groups, bias, 
+                  pool_type, pool_size, pool_stride, batch_norm, batch_norm_learn, pad_front, device, dtype):
             Constructor method for initializing the CNN1D module and its attributes.
 
         forward(self, input):
@@ -36,10 +40,12 @@ class CNN1D(torch.nn.Module):
         output = model(X)
     """
     def __init__(self, 
-                 in_channels, out_channels, input_len = 1,
+                 in_channels, out_channels, input_len=1,
+                 pad_front=False,
                  kernel_size=[(1,)], kernel_stride=[(1,)], padding=[(0,)], 
                  dilation=[(1,)], groups=[1], bias=[False], 
-                 pool_type=[None], pool_size=[(2,)], pool_stride = [(1,)],
+                 pool_type=[None], pool_size=[(2,)], pool_stride=[(1,)],
+                 batch_norm=False, batch_norm_learn=False,
                  device=None, dtype=None):
         """
         Constructor method for initializing the CNN1D module and its attributes.
@@ -56,12 +62,16 @@ class CNN1D(torch.nn.Module):
             pool_type (list, optional): List of strings specifying the pooling type for each layer. 
                                         Options: [None, 'max', 'avg']. Default is [None].
             pool_size (list, optional): List of tuples specifying the pooling size for each layer. Default is [2].
-            flatten (bool, optional): If True, the output tensor will be flattened. Default is False.
+            pool_stride (list, optional): List of tuples specifying the pooling stride for each layer. Default is [(1,)].
+            batch_norm (bool, optional): If True, applies batch normalization after each convolutional layer. Default is False.
+            batch_norm_learn (bool, optional): If True, allows the batch normalization layers to learn affine parameters. Default is False.
+            pad_front (bool, optional): If True, pads the front of the input tensor with zeros before applying the convolutional layers. Default is False.
             device (str, optional): Device on which the model parameters should be stored. Default is None.
             dtype (torch.dtype, optional): Data type for the model parameters. Default is None.
         """
         super(CNN1D, self).__init__()
-        
+
+        # Store the arguments as attributes
         locals_ = locals().copy()
         for arg in locals_:
             if arg != 'self':
@@ -69,6 +79,7 @@ class CNN1D(torch.nn.Module):
 
         self.num_layers = len(out_channels)
 
+        # Replicate single-value parameters to match the number of layers
         if len(self.kernel_size) == 1: self.kernel_size = self.kernel_size * self.num_layers
         if len(self.kernel_stride) == 1: self.kernel_stride = self.kernel_stride * self.num_layers
         if len(self.padding) == 1: self.padding = self.padding * self.num_layers
@@ -79,12 +90,15 @@ class CNN1D(torch.nn.Module):
         if len(self.pool_size) == 1: self.pool_size = self.pool_size * self.num_layers
         if len(self.pool_stride) == 1: self.pool_stride = self.pool_stride * self.num_layers
 
+        # Create the CNN layers
         self.cnn = torch.nn.ModuleList()  
         for i in range(self.num_layers):
             self.cnn.append(torch.nn.Sequential())
 
+            # Determine the input channels for the current layer
             in_channels_i = self.in_channels if i == 0 else self.out_channels[i - 1]
 
+            # Add Conv1d layer to the current layer
             self.cnn[-1].append(torch.nn.Conv1d(in_channels=in_channels_i,
                                                 out_channels=self.out_channels[i],
                                                 kernel_size=self.kernel_size[i],
@@ -95,19 +109,28 @@ class CNN1D(torch.nn.Module):
                                                 bias=self.bias[i],
                                                 device=self.device, dtype=self.dtype))
 
+            # Add batch normalization layer if specified
+            if self.batch_norm:
+                batch_norm_i = torch.nn.BatchNorm1d(self.out_channels[i], affine=self.batch_norm_learn)
+            else:
+                batch_norm_i = torch.nn.Identity()
+
+            self.cnn[-1].append(batch_norm_i)
+
+            # Add pooling layer if specified
             if self.pool_type[i] is None:
                 pool_i = torch.nn.Identity()
             if self.pool_type[i] == 'max':
-                pool_i = torch.nn.MaxPool1d(kernel_size = self.pool_size[i],
-                                            stride = self.pool_stride[i])
+                pool_i = torch.nn.MaxPool1d(kernel_size=self.pool_size[i],
+                                            stride=self.pool_stride[i])
             elif self.pool_type[i] == 'avg':
-                pool_i = torch.nn.AvgPool1d(kernel_size = self.pool_size[i],
-                                            stride = self.pool_stride[i])     
+                pool_i = torch.nn.AvgPool1d(kernel_size=self.pool_size[i],
+                                            stride=self.pool_stride[i])     
 
             self.cnn[-1].append(pool_i)
 
-        X = torch.zeros((1,self.input_len, in_channels)).to(device = self.device, dtype = self.dtype)
-                     
+        # Determine the number of output features after passing through the layers
+        X = torch.zeros((1, self.input_len, in_channels)).to(device=self.device, dtype=self.dtype)
         self.out_features = self.forward(X).shape[-1]
 
     def forward(self, input):
@@ -122,8 +145,11 @@ class CNN1D(torch.nn.Module):
         """
         output = input.clone()
         for i in range(self.num_layers):   
-            input_i = torch.nn.functional.pad(output.transpose(1, 2), (self.kernel_size[i][0] - 1, 0))
-            output = self.cnn[i][0](input_i)
-            output = self.cnn[i][1](output).transpose(1, 2)
+            # Apply padding to the input tensor if pad_front is True
+            input_i = torch.nn.functional.pad(output.transpose(1, 2), (self.kernel_size[i][0] - 1, 0)) if self.pad_front else output.transpose(1, 2)
+            # Apply the current CNN layer to the input tensor
+            output = self.cnn[i][1](self.cnn[i][0](input_i))
+            # Transpose back the output tensor to the original shape
+            output = self.cnn[i][2](output).transpose(1, 2)
 
         return output
