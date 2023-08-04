22ee8aa4112996c63284ddd8c0fa5cb5c5751320
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-04 12:08:40 -0400

    Update SequenceModelBase.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceModelBase.py
+++ ts_src/SequenceModelBase.py
@@ -344,9 +344,9 @@ class SequenceModelBase(torch.nn.Module):
                                          device = self.device, dtype = self.dtype)
 
     with torch.no_grad():            
-      X = torch.empty((1, self.input_len, input_size)).to(device = self.device,
+      X = torch.empty((2, self.input_len, input_size)).to(device = self.device,
                                                           dtype = self.dtype)
-      encoder_output = torch.empty((1,self.input_len,encoder_output_size)).to(device = self.device,
+      encoder_output = torch.empty((2,self.input_len,encoder_output_size)).to(device = self.device,
                                                                               dtype = self.dtype) if encoder_output_size is not None else None
       
       self.output_len = self.forward(X, encoder_output = encoder_output)[0].shape[1]
