88b5514297056cd8589f839a152630846d16e654
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-07 10:24:17 -0400

    Update ExploratoryTimeSeriesAnalysis.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/ExploratoryTimeSeriesAnalysis.py
+++ ts_src/ExploratoryTimeSeriesAnalysis.py
@@ -18,7 +18,10 @@ class ExploratoryTimeSeriesAnalysis():
     dtype (torch.dtype, optional): Data type to be used for torch tensors. Defaults to None.
   """
 
-  def __init__(self, data, time_name, dt, time_unit, device=None, dtype=None):
+  def __init__(self,
+               data, data_names, time_name, dt, time_unit,
+               data_units = None,
+               device=None, dtype=None):
     """
     Initialize an instance of ExploratoryTimeSeriesAnalysis.
 
@@ -35,13 +38,11 @@ class ExploratoryTimeSeriesAnalysis():
     for arg in locals_:
       setattr(self, arg, locals_[arg])
 
-  def generate_xcorr(self, data_names=None, hann_window_len=[None], demean = [False]):
+  def generate_xcorr(self, hann_window_len=[None], demean = [False]):
     """
     Generate cross-correlation for the specified data columns.
 
     Args:
-      data_names (list, optional): List of column names for which cross-correlation needs to be computed.
-                                    Defaults to None (computes for all columns except the time column).
       hann_window_len (list, optional): List of window lengths for applying Hann window to the data.
                                         If a single value is provided, it will be used for all columns.
                                         Defaults to [None] (no Hann window is applied).
@@ -51,18 +52,17 @@ class ExploratoryTimeSeriesAnalysis():
     """
 
     if len(hann_window_len) == 1:
-      hann_window_len = hann_window_len * len(data_names)
+      hann_window_len = hann_window_len * len(self.data_names)
 
     if len(demean) == 1:
-      demean = demean * len(data_names)  
+      demean = demean * len(self.data_names)
 
-    data_names = [name for name in self.data if name != self.time_name] if data_names is None else data_names
     data = self.data.copy()
 
-    self.record_len = data[data_names[0]].shape[0]
+    self.record_len = data[self.data_names[0]].shape[0]
 
     data_ = []
-    for i, name in enumerate(data_names):
+    for i, name in enumerate(self.data_names):
       data_.append(torch.tensor(self.data[name]).to(device=self.device, dtype=self.dtype)
                     if not isinstance(self.data[name], torch.Tensor) else self.data[name].to(device=self.device,
                                                                                           dtype=self.dtype))
@@ -79,20 +79,19 @@ class ExploratoryTimeSeriesAnalysis():
     xcorr_ = torch.nn.functional.conv1d(data, data, padding=(self.record_len - 1))
 
     self.xcorr = {}
-    for i1, name1 in enumerate(data_names):
-      for i2, name2 in enumerate(data_names):
+    for i1, name1 in enumerate(self.data_names):
+      for i2, name2 in enumerate(self.data_names):
         self.xcorr[f"{name2}[t]*{name1}[t-lag]"] = xcorr_[i1, i2].flip(0)[-self.record_len:] / self.record_len
 
-    self.lags = torch.arange(self.record_len) * self.dt
+    self.lags = np.arange(self.record_len) * self.dt
 
-  def plot_ts(self, data_names=None, domain='time', hann_window_len=[None], demean = [False],
-              data_units=None, xlim=None,
+  def plot_ts(self, domain='time', hann_window_len=[None], demean = [False],
+              xlim=None,
               figsize=None, title_size=20, xlabel_size=20, ylabel_size=20, fig_num=1):
     """
     Plot the time series data.
 
     Args:
-      data_names (list, optional): List of column names to be plotted. Defaults to None (plots all columns).
       domain (str, optional): The domain of the plot, 'time' or 'frequency'. Defaults to 'time'.
       hann_window_len (list, optional): List of window lengths for applying Hann window to the data.
                                         If a single value is provided, it will be used for all columns.
@@ -108,26 +107,25 @@ class ExploratoryTimeSeriesAnalysis():
     Returns:
       None
     """
-    
+
     if len(hann_window_len) == 1:
-      hann_window_len = hann_window_len * len(data_names)
+      hann_window_len = hann_window_len * len(self.data_names)
     if len(demean) == 1:
-      demean = demean * len(data_names)  
+      demean = demean * len(self.data_names)
 
     data, input_size = [], []
-    for name in data_names:
+    for name in self.data_names:
       data_ = self.data[name]
       if not isinstance(data_, torch.Tensor):
         data_ = torch.tensor(data_)
       data_ = data_.to(device = self.device, dtype = self.dtype)
-      
+
       data.append(data_)
       input_size.append(data_.shape[-1])
 
     data = torch.cat(data, -1)
 
-    data_names = [name for name in data if name != self.time_name] if data_names is None else data_names
-    num_data = len(data_names)
+    num_data = len(self.data_names)
 
     self.record_len = data.shape[0]
 
@@ -138,7 +136,7 @@ class ExploratoryTimeSeriesAnalysis():
     elif domain == 'frequency':
 
       data_ = []
-      for i, name in enumerate(data_names):
+      for i, name in enumerate(self.data_names):
         data_.append(torch.tensor(self.data[name]).to(device=self.device, dtype=self.dtype)
                       if not isinstance(self.data[name], torch.Tensor) else self.data[name].to(device=self.device,
                                                                                           dtype=self.dtype))
@@ -157,15 +155,15 @@ class ExploratoryTimeSeriesAnalysis():
     fig, ax = plt.subplots(num_data, 1, figsize=figsize, num=fig_num)
 
     j = 0
-    for i, name in enumerate(data_names):
+    for i, name in enumerate(self.data_names):
       ax_i = ax[i] if num_data > 1 else ax
-      ax_i.plot(xaxis, data[:, j:(j + input_size[i])], 'k', label=name)
+      ax_i.plot(xaxis, data[:, j:(j + input_size[i])], 'b', label=name)
       ax_i.set_title(name, fontsize=title_size)
       ax_i.legend()
       if i == num_data - 1:
         ax_i.set_xlabel(xlabel, fontsize=xlabel_size)
-      if data_units is not None:
-        ax_i.set_ylabel(data_units[i], fontsize=ylabel_size)
+      if self.data_units is not None:
+        ax_i.set_ylabel(self.data_units[i], fontsize=ylabel_size)
       ax_i.set_xlim(xlim)
       ax_i.grid()
       ax_i.legend(loc='upper right')
@@ -174,7 +172,7 @@ class ExploratoryTimeSeriesAnalysis():
 
     fig.tight_layout()
 
-  def plot_xcorr(self, domain='time', data_units=None, xlim=None, figsize=None,
+  def plot_xcorr(self, data_names = None, domain='time', xlim=None, figsize=None,
                   title_size=20, xlabel_size=20, ylabel_size=20, fig_num=1):
       """
       Plot the cross-correlation between different data columns.
@@ -193,32 +191,42 @@ class ExploratoryTimeSeriesAnalysis():
         None
       """
 
-      lags = self.lags
-      fig, ax = plt.subplots(int(np.sqrt(len(self.xcorr))), int(np.sqrt(len(self.xcorr))), figsize=figsize, num=fig_num)
+      data_names = data_names or self.data_names
+
+      fig, ax = plt.subplots(len(self.data_names), len(data_names), figsize=figsize, num=fig_num)
 
       xcorr_names = list(self.xcorr)
 
       i = -1
-      
+
+      jy = -1
       for ix in range(int(np.sqrt(len(self.xcorr)))):
         for iy in range(int(np.sqrt(len(self.xcorr)))):
-          ax_ixiy = ax[ix, iy] if int(np.sqrt(len(self.xcorr))) > 1 else ax
-
           i += 1
           xcorr_ = self.xcorr[xcorr_names[i]].clone()
+          if any(f"{name}[t]" in xcorr_names[i] for name in data_names):
 
-          if domain == 'frequency':
-            xaxis, xcorr_, _ = fft(xcorr_, fs=1 / self.dt)
-            xlabel = f"Frequency [1/{self.time_unit}]"
-          else:
-            xaxis = lags
-            xlabel = f"Lags [{self.time_unit}]"
+            jy += 1
+            ax_ixiy = ax[ix, jy] if len(data_names) > 1 else ax[jy]
 
-          ax_ixiy.plot(xaxis, xcorr_)
-          ax_ixiy.set_title(xcorr_names[i], fontsize=title_size)
-          if ix == int(np.sqrt(len(self.xcorr))) - 1:
-            ax_ixiy.set_xlabel(xlabel, fontsize=xlabel_size)
-          ax_ixiy.set_xlim(xlim)
-          ax_ixiy.grid()
+            if domain == 'frequency':
+              xaxis, xcorr_, _ = fft(xcorr_, fs=1 / self.dt)
+              xlabel = f"Frequency [1/{self.time_unit}]"
+              if self.data_units is not None:
+                ylabel = f"{self.data_units[ix]}^2*{self.time_unit}" if ix == iy else f"{self.data_units[ix]}*{self.data_units[iy]}/Hz"
+            else:
+              xaxis = self.lags
+              xlabel = f"Lags [{self.time_unit}]"
+              if self.data_units is not None:
+                ylabel = f"{self.data_units[ix]}^2" if ix == iy else f"{self.data_units[ix]}*{self.data_units[iy]}"
+
+            ax_ixiy.plot(xaxis, xcorr_, 'b')
+            ax_ixiy.set_title(xcorr_names[i], fontsize=title_size)
+            if ix == int(np.sqrt(len(self.xcorr))) - 1:
+              ax_ixiy.set_xlabel(xlabel, fontsize=xlabel_size)
+            ax_ixiy.set_ylabel(ylabel, fontsize=ylabel_size)
+
+            ax_ixiy.set_xlim(xlim)
+            ax_ixiy.grid()
 
       fig.tight_layout()
