c41c1e65f8445dffee63d5fe6ee2a329de0adce9
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-08 18:19:19 -0400

    Update ARIMA.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/ARIMA.py
+++ ts_src/ARIMA.py
@@ -1,11 +1,14 @@
 import statsmodels as sm
 import numpy as np
 
+from ts_src.Criterion import Criterion
+
 class ARIMA():
   def __init__(self,
-               df_train, df_test, 
+               df_train, df_test,
                endog_name, 
-               exog_name = None, order = (0, 0, 0)):
+               exog_name = None, order = (0, 0, 0),
+               loss = 'mse', metric = 'mae'):
 
     locals_ = locals().copy()
     for arg in locals_:
@@ -15,36 +18,72 @@ class ARIMA():
     self.model = sm.tsa.arima.model.ARIMA(endog = df_train[endog_name], 
                                           exog = df_train[exog_name] if exog_name is not None else None,                              
                                           order = self.order)
-  
-  def fit(self):
+
+    self.loss_fn = Criterion(self.loss) if self.loss is not None else None
+    self.metric_fn = Criterion(self.metric) if self.metric is not None else None
+
+  def fit(self, transforms = None):
+    
+    if transforms is not None:
+      for name in [self.endog_name, self.exog_name]:
+        if name in transforms:
+          self.df_train[name] = transforms[name].fit_transform(self.df_train[name])
+          self.df_test[name] = transforms[name].transform(self.df_test[name])
 
     self.results = model.fit()
   
-  def predict(self):
+  def predict(self, transforms = None):
 
     if not hasattr(self, 'results'): self.fit()
 
     p, d, q = self.order
 
-    # train
-    self.df_train[f"{self.endog_name}_prediction"] = np.full((self.df_train.shape[0],), np.nan) 
+    df_train, df_test = self.df_train.copy(), self.df_test.copy()
 
-    train_target = np.pad(self.df_train[self.endog_name].values, (p, 0), mode = 'constant') 
+    # train
+    df_train[f"{self.endog_name}_prediction"] = np.full((df_train.shape[0],), np.nan) 
+
+    train_target = np.pad(df_train[self.endog_name].values, (p, 0), mode = 'constant') 
 
     for n in range(p, train_target.shape[0]):
-      self.df_train[f"{self.endog_name}_prediction"][n-p] = np.dot(np.flip(train_target[(n-p):n], axis=0), self.results.arparams)
+      df_train[f"{self.endog_name}_prediction"][n-p] = np.dot(np.flip(train_target[(n-p):n], axis=0), self.results.arparams)
     #
 
     # test
-    self.df_test[f"{self.endog_name}_prediction"] = np.full((self.df_test.shape[0],), np.nan) 
+    df_test[f"{self.endog_name}_prediction"] = np.full((df_test.shape[0],), np.nan) 
 
-    test_target = np.pad(self.df_test[self.endog_name].values, (p, 0), mode = 'constant') 
+    test_target = np.pad(df_test[self.endog_name].values, (p, 0), mode = 'constant') 
 
     for n in range(p, test_target.shape[0]):
-      self.df_test[f"{self.endog_name}_prediction"][n-p] = np.dot(np.flip(test_target[(n-p):n], axis=0), self.results.arparams)
+      df_test[f"{self.endog_name}_prediction"][n-p] = np.dot(np.flip(test_target[(n-p):n], axis=0), self.results.arparams)
     #
+
+    if transforms is not None:
+      if self.endog_name in transforms:
+        df_train[self.endog_name] = transforms[self.endog_name].inverse_transform(df_train[self.endog_name])
+        df_train[f"{self.endog_name}_prediction"] = transforms[self.endog_name].inverse_transform(df_train[f"{self.endog_name}_prediction"])
+
+        df_test[self.endog_name] = transforms[self.endog_name].inverse_transform(df_test[self.endog_name])
+        df_test[f"{self.endog_name}_prediction"] = transforms[self.endog_name].inverse_transform(df_test[f"{self.endog_name}_prediction"])
+
+    if self.loss_fn is not None:
+      df_train[f"{self.endog_name}_{self.loss_fn.name}"] = self.loss_fn(df_train[f"{self.endog_name}_prediction"],
+                                                                        df_train[self.endog_name])
+      df_test[f"{self.endog_name}_{self.loss_fn.name}"] = self.loss_fn(df_test[f"{self.endog_name}_prediction"],
+                                                                       df_test[self.endog_name])
+      
+    if self.metric_fn is not None:
+      df_train[f"{self.endog_name}_{self.metric_fn.name}"] = self.metric_fn(df_train[f"{self.endog_name}_prediction"],
+                                                                            df_train[self.endog_name])
+      df_test[f"{self.endog_name}_{self.metric_fn.name}"] = self.metric_fn(df_test[f"{self.endog_name}_prediction"],
+                                                                           df_test[self.endog_name])
     
-  def forecast(self, num_forecast_steps = 1, input = None):
+    return df_train, df_test
+
+  def forecast(self, 
+               num_forecast_steps = 1,
+               input = None,
+               transforms = None):
 
     if not hasattr(self, 'results'): self.fit()
     
@@ -70,4 +109,8 @@ class ARIMA():
 
     forecast = np.concatenate(forecast, 1)
 
+    if transforms is not None:
+      if self.endog_name in transforms:
+        forecast = transforms[self.endog_name].inverse_transform(forecast)
+        
     return forecast
