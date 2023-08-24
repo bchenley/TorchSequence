b22432e1a00c960c63f27da9c84987b3bb1f98e7
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-24 00:36:10 -0400

    Update SequenceDataset.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceDataset.py
+++ ts_src/SequenceDataset.py
@@ -1,232 +1,230 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
 
-from ts_src.moving_average import moving_average
-from ts_src.fft import fft
+class SequenceDataset(torch.utils.data.Dataset):
 
-class ExploratoryTimeSeriesAnalysis():
-  """
-  A class for performing exploratory time series analysis.
+  '''
+  Dataset class for sequence data.
 
-  Parameters:
-    data (dict): Dictionary containing the time series data.
-    time_name (str): Name of the time column in the data.
-    dt (float): Time step or sampling interval.
-    time_unit (str): Unit of time (e.g., 'seconds', 'days').
-    device (str, optional): Device to be used for computations (e.g., 'cuda', 'cpu'). Defaults to None.
-    dtype (torch.dtype, optional): Data type to be used for torch tensors. Defaults to None.
-  """
+  Args:
+    data (dict): Dictionary containing input and output data.
+    input_names (list): Names of the input data.
+    output_names (list): Names of the output data.
+    step_name (str): Name of the step data.
+    input_len (list): List of input sequence lengths. If a single value is provided, it is replicated for all inputs.
+    output_len (list): List of output sequence lengths. If a single value is provided, it is replicated for all outputs.
+    shift (list): List of output shifts. If a single value is provided, it is replicated for all outputs.
+    stride (int): Stride value.
+    init_input (torch.Tensor or None): Initial input for padding. Defaults to None.
+    print_summary (bool): Whether to print summary information. Defaults to False.
+    device (str): Device on which the dataset is allocated. Defaults to 'cpu'.
+    dtype (torch.dtype): Data type of the dataset. Defaults to torch.float32.
+    forecast (bool): Whether the dataset is for forecasting. Defaults to False.
+  '''
 
   def __init__(self,
-               data, data_names, time_name, dt, time_unit,
-               data_units = None,
-               device=None, dtype=None):
-    """
-    Initialize an instance of ExploratoryTimeSeriesAnalysis.
-
-    Args:
-      data (dict): Dictionary containing the time series data.
-      time_name (str): Name of the time column in the data.
-      dt (float): Time step or sampling interval.
-      time_unit (str): Unit of time (e.g., 'seconds', 'days').
-      device (str, optional): Device to be used for computations (e.g., 'cuda', 'cpu'). Defaults to None.
-      dtype (torch.dtype, optional): Data type to be used for torch tensors. Defaults to None.
-    """
+               data: dict,
+               input_names, output_names, step_name='steps',
+               input_len=[1], output_len=[1], shift=[0], stride=1,
+               init_input=None,
+               shuffle_batch = False,
+               forecast = False,
+               print_summary=False,
+               device='cpu', dtype=torch.float32):
 
     locals_ = locals().copy()
+
     for arg in locals_:
-      setattr(self, arg, locals_[arg])
+      if arg != 'self':
+        if arg == 'data':
+          setattr(self, arg, locals_[arg].copy())
+        else:
+          setattr(self, arg, locals_[arg])
 
-  def generate_xcorr(self, hann_window_len=[None], demean = [False]):
-    """
-    Generate cross-correlation for the specified data columns.
+    self.num_inputs, self.num_outputs = len(self.input_names), len(self.output_names)
 
-    Args:
-      hann_window_len (list, optional): List of window lengths for applying Hann window to the data.
-                                        If a single value is provided, it will be used for all columns.
-                                        Defaults to [None] (no Hann window is applied).
+    if len(self.input_len) == 1:
+        self.input_len = self.input_len * self.num_inputs
 
-    Returns:
-      None
-    """
+    if len(self.output_len) == 1:
+        self.output_len = self.output_len * self.num_outputs
+    if len(self.shift) == 1:
+        self.shift = self.shift * self.num_outputs
+    
+    for name in self.input_names + self.output_names:
+      if not isinstance(self.data[name], torch.Tensor):
+        self.data[name] = torch.tensor(self.data[name]).to(device = self.device,
+                                                           dtype = self.dtype)
 
-    if len(hann_window_len) == 1:
-      hann_window_len = hann_window_len * len(self.data_names)
+    self.data_len = self.data[self.input_names[0]].shape[0]
 
-    if len(demean) == 1:
-      demean = demean * len(self.data_names)
+    if step_name not in data: self.data[step_name] = torch.arange(self.data_len).to(device = self.device,
+                                                                                    dtype = torch.long)
 
-    data = self.data.copy()
+    self.input_len = [self.data_len if len == -1 else len for len in self.input_len]
+    self.output_len = [np.max(self.input_len) if len == -1 else len for len in self.output_len]
 
-    self.record_len = data[self.data_names[0]].shape[0]
+    self.input_size = [self.data[name].shape[-1] for name in self.input_names]
+    self.output_size = [self.data[name].shape[-1] for name in self.output_names]
 
-    data_ = []
-    for i, name in enumerate(self.data_names):
-      data_.append(torch.tensor(self.data[name]).to(device=self.device, dtype=self.dtype)
-                    if not isinstance(self.data[name], torch.Tensor) else self.data[name].to(device=self.device,
-                                                                                          dtype=self.dtype))
+    self.max_input_len = np.max(self.input_len)
+    self.max_output_len = np.max(self.output_len)
+    self.max_shift = np.max(self.shift)
 
-      data_[-1] = data_[-1] - moving_average(data_[-1], torch.hann_window(hann_window_len[i]).to(data_[-1])) \
-          if hann_window_len[i] is not None else data_[-1]
+    self.has_ar = np.isin(self.output_names, self.input_names).any()
 
-      data_[-1] = data_[-1] - data_[-1].mean(0) if demean[i] else data_[-1]
-
-    data = torch.cat(data_, -1)
-
-    data = data.t().unsqueeze(1)
-
-    xcorr_ = torch.nn.functional.conv1d(data, data, padding=(self.record_len - 1))
-
-    self.xcorr = {}
-    for i1, name1 in enumerate(self.data_names):
-      for i2, name2 in enumerate(self.data_names):
-        self.xcorr[f"{name2}[t]*{name1}[t-lag]"] = xcorr_[i1, i2].flip(0)[-self.record_len:] / self.record_len
-
-    self.lags = np.arange(self.record_len) * self.dt
-
-  def plot_ts(self, domain='time', hann_window_len=[None], demean = [False],
-              xlim=None,
-              figsize=None, title_size=20, xlabel_size=20, ylabel_size=20, fig_num=1):
-    """
-    Plot the time series data.
-
-    Args:
-      domain (str, optional): The domain of the plot, 'time' or 'frequency'. Defaults to 'time'.
-      hann_window_len (list, optional): List of window lengths for applying Hann window to the data.
-                                        If a single value is provided, it will be used for all columns.
-                                        Defaults to [None] (no Hann window is applied).
-      data_units (list, optional): List of units for the data columns. Defaults to None.
-      xlim (tuple, optional): Limits for the x-axis. Defaults to None (determined automatically).
-      figsize (tuple, optional): Figure size in inches. Defaults to None (determined automatically).
-      title_size (int, optional): Font size for the plot titles. Defaults to 20.
-      xlabel_size (int, optional): Font size for the x-axis label. Defaults to 20.
-      ylabel_size (int, optional): Font size for the y-axis label. Defaults to 20.
-      fig_num (int, optional): Figure number. Defaults to 1.
-
-    Returns:
-      None
-    """
-
-    if len(hann_window_len) == 1:
-      hann_window_len = hann_window_len * len(self.data_names)
-    if len(demean) == 1:
-      demean = demean * len(self.data_names)
-
-    data, input_size = [], []
-    for name in self.data_names:
-      data_ = self.data[name]
-      if not isinstance(data_, torch.Tensor):
-        data_ = torch.tensor(data_)
-      data_ = data_.to(device = self.device, dtype = self.dtype)
-
-      data.append(data_)
-      input_size.append(data_.shape[-1])
-
-    data = torch.cat(data, -1)
-
-    num_data = len(self.data_names)
-
-    self.record_len = data.shape[0]
-
-    if domain == 'time':
-      self.time = self.data[self.time_name]
-      xaxis = self.time
-      xlabel = f"Time [{self.time_unit}]"
-    elif domain == 'frequency':
-
-      data_ = []
-      for i, name in enumerate(self.data_names):
-        data_.append(torch.tensor(self.data[name]).to(device=self.device, dtype=self.dtype)
-                      if not isinstance(self.data[name], torch.Tensor) else self.data[name].to(device=self.device,
-                                                                                          dtype=self.dtype))
-
-        data_[-1] = data_[-1] - moving_average(data_[-1], torch.hann_window(hann_window_len[i]).to(data_[-1])) \
-                    if hann_window_len[i] is not None else data_[-1]
-
-        data_[-1] = data_[-1] - data_[-1].mean(0) if demean[i] else data_[-1]
-
-      data = torch.cat(data_, -1)
-
-      self.freq, data, _ = fft(data, fs=1 / self.dt)
-      xaxis = self.freq
-      xlabel = f"Frequency [1/{self.time_unit}]"
-
-    fig, ax = plt.subplots(num_data, 1, figsize=figsize, num=fig_num)
-
-    j = 0
-    for i, name in enumerate(self.data_names):
-      ax_i = ax[i] if num_data > 1 else ax
-      ax_i.plot(xaxis, data[:, j:(j + input_size[i])], 'b', label=name)
-      ax_i.set_title(name, fontsize=title_size)
-      ax_i.legend()
-      if i == num_data - 1:
-        ax_i.set_xlabel(xlabel, fontsize=xlabel_size)
-      if self.data_units is not None:
-        ax_i.set_ylabel(self.data_units[i], fontsize=ylabel_size)
-      ax_i.set_xlim(xlim)
-      ax_i.grid()
-      ax_i.legend(loc='upper right')
-
-      j += input_size[i]
-
-    fig.tight_layout()
-
-  def plot_xcorr(self, data_names = None, domain='time', xlim=None, figsize=None,
-                  title_size=20, xlabel_size=20, ylabel_size=20, fig_num=1):
-      """
-      Plot the cross-correlation between different data columns.
-
-      Args:
-        domain (str, optional): The domain of the plot, 'time' or 'frequency'. Defaults to 'time'.
-        data_units (list, optional): List of units for the cross-correlation values. Defaults to None.
-        xlim (tuple, optional): Limits for the x-axis. Defaults to None (determined automatically).
-        figsize (tuple, optional): Figure size in inches. Defaults to None (determined automatically).
-        title_size (int, optional): Font size for the plot titles. Defaults to 20.
-        xlabel_size (int, optional): Font size for the x-axis label. Defaults to 20.
-        ylabel_size (int, optional): Font size for the y-axis label. Defaults to 20.
-        fig_num (int, optional): Figure number. Defaults to 1.
-
-      Returns:
-        None
-      """
-
-      data_names = data_names or self.data_names
-
-      fig, ax = plt.subplots(len(self.data_names), len(data_names), figsize=figsize, num=fig_num)
+    self.input_window_idx = []
+    for i in range(self.num_inputs):
+      input_window_idx_i = torch.arange(self.max_input_len - self.input_len[i], self.max_input_len).to(device = 'cpu',
+                                                                                                               dtype = torch.long)
       
-      xcorr_names = list(self.xcorr)
+      input_window_idx_i += int(self.has_ar & (self.input_names[i] not in self.output_names))
 
-      i = -1
+      self.input_window_idx.append(input_window_idx_i)
 
-      jy = -1
-      for ix in range(int(np.sqrt(len(self.xcorr)))):
-        for iy in range(int(np.sqrt(len(self.xcorr)))):
-          i += 1
-          xcorr_ = self.xcorr[xcorr_names[i]].clone()
-          if any(f"{name}[t]" in xcorr_names[i] for name in data_names):
 
-            jy += 1
-            ax_ixiy = ax[ix, jy] if len(data_names) > 1 else ax[jy] if len(self.data_names) > 1 else ax
+    self.output_window_idx = []
+    for i in range(self.num_outputs):
+      output_window_idx_i = torch.arange(self.max_input_len - self.output_len[i], self.max_input_len).to(device = 'cpu',
+                                                                                                         dtype = torch.long) + self.shift[i]
 
-            if domain == 'frequency':
-              xaxis, xcorr_, _ = fft(xcorr_, fs=1 / self.dt)
-              xlabel = f"Frequency [1/{self.time_unit}]"
-              if self.data_units is not None:
-                ylabel = f"{self.data_units[ix]}^2*{self.time_unit}" if ix == iy else f"{self.data_units[ix]}*{self.data_units[iy]}/Hz"
-            else:
-              xaxis = self.lags
-              xlabel = f"Lags [{self.time_unit}]"
-              if self.data_units is not None:
-                ylabel = f"{self.data_units[ix]}^2" if ix == iy else f"{self.data_units[ix]}*{self.data_units[iy]}"
+      output_window_idx_i += int(self.has_ar)
 
-            ax_ixiy.plot(xaxis, xcorr_, 'b')
-            ax_ixiy.set_title(xcorr_names[i], fontsize=title_size)
-            if ix == int(np.sqrt(len(self.xcorr))) - 1:
-              ax_ixiy.set_xlabel(xlabel, fontsize=xlabel_size)
-            ax_ixiy.set_ylabel(ylabel, fontsize=ylabel_size)
+      self.output_window_idx.append(output_window_idx_i)
 
-            ax_ixiy.set_xlim(xlim)
-            ax_ixiy.grid()
+    self.total_window_size = torch.cat(self.output_window_idx).max().item() + 1
+    self.total_window_idx = torch.arange(self.total_window_size).to(device = 'cpu',
+                                                                    dtype = torch.long)
 
-      fig.tight_layout()
+    self.start_step = np.max([0, (self.max_input_len - self.max_output_len + self.max_shift)]).item()
+
+    if self.print_summary:
+      print('\n'.join([f'Data length: {self.data_len}',
+                       f'Window size: {self.total_window_size}',
+                       f'Step indices: {self.total_window_idx.tolist()}',
+                       '\n'.join([f'Input indices for {self.input_names[i]}: {self.input_window_idx[i].tolist()}' for i in range(self.num_inputs)]),
+                       '\n'.join([f'Output indices for {self.output_names[i]}: {self.output_window_idx[i].tolist()}' for i in range(self.num_outputs)])]))
+
+    if self.forecast:
+      pad_size = self.total_window_size - self.max_input_len # + int(self.has_ar)
+
+      self.data[self.step_name] = torch.cat((self.data[self.step_name][-self.max_input_len:],
+                                             torch.arange(pad_size).to(device = self.device, dtype = torch.long) + self.data[self.step_name].max() + 1)).to(device = self.device,
+                                                                                                                                                            dtype = torch.long)
+      for name in np.unique(self.input_names + self.output_names):
+        data_size = self.data[name].shape[-1]
+        self.data[name] = self.data[name][-self.max_input_len:]
+        self.data[name] = torch.nn.functional.pad(self.data[name],
+                                                  pad = (0, 0, 0, pad_size),
+                                                  mode = 'constant', value = 0.)
+
+      self.data_len = len(self.data[self.step_name])
+
+    self.input_samples, self.output_samples, self.steps_samples, self.id = self.get_samples()
+
+  def get_samples(self):
+
+    '''
+    Generates input, output, and steps samples for the dataset.
+
+    Returns:
+        tuple: A tuple containing input samples, output samples, and steps samples.
+    '''
+
+    input_samples, output_samples, steps_samples = [], [], []
+
+    unique_input_window_idx = torch.cat(self.input_window_idx).unique()
+    unique_output_window_idx = torch.cat(self.output_window_idx).unique()
+
+    min_output_idx = torch.cat(self.output_window_idx).min().item()
+
+    window_idx_n = self.total_window_idx
+
+    num_samples = 0
+    while window_idx_n.max() < self.data_len:
+        num_samples += 1
+
+        steps_samples.append(self.data[self.step_name][window_idx_n])
+
+        # input
+        input_n = torch.zeros((self.max_input_len, np.sum(self.input_size))).to(device=self.device,
+                                                                           dtype=self.dtype)
+
+        j = 0
+        for i in range(self.num_inputs):
+          input_window_idx_i = self.input_window_idx[i]
+
+          input_samples_window_idx_i = window_idx_n[input_window_idx_i] # - int(self.input_names[i] in self.output_names)
+
+          if (input_samples_window_idx_i[0] == 0) & (self.init_input is not None):
+            input_n[0, j:(j + self.input_size[i])] = self.init_input[j:(j + self.input_size[i])]
+
+          # input_window_idx_i = input_window_idx_i[input_samples_window_idx_i >= 0]
+          # input_samples_window_idx_i = input_samples_window_idx_i[input_samples_window_idx_i >= 0]
+
+          input_n[input_window_idx_i, j:(j + self.input_size[i])] = self.data[self.input_names[i]].clone()[input_samples_window_idx_i]
+
+          j += self.input_size[i]
+
+        input_samples.append(input_n)
+
+        # output
+        output_n = torch.zeros((len(unique_output_window_idx), np.sum(self.output_size))).to(device = self.device,
+                                                                                             dtype = self.dtype)
+
+        j = 0
+        for i in range(self.num_outputs):
+          output_window_idx_i = self.output_window_idx[i]
+
+          output_samples_window_idx_i = window_idx_n[output_window_idx_i]
+
+          output_window_idx_j = output_window_idx_i - min_output_idx
+
+          output_n[output_window_idx_j, j:(j + self.output_size[i])] = self.data[self.output_names[i]].clone()[output_samples_window_idx_i]
+
+          j += self.output_size[i]
+
+        output_samples.append(output_n)
+
+        window_idx_n = num_samples * self.stride + self.total_window_idx
+
+    input_samples = torch.stack(input_samples)
+    output_samples = torch.stack(output_samples)
+    steps_samples = torch.stack(steps_samples)
+
+    if self.forecast:
+      input_samples = input_samples[-1:]
+      output_samples = output_samples[-1:]
+      steps_samples = steps_samples[-1:]
+      num_samples = 1
+
+    self.num_samples = num_samples
+
+    self.batch_shuffle_idx = None
+    if self.shuffle_batch:
+      self.batch_shuffle_idx = torch.randperm(self.num_samples)
+      input_samples, output_samples, steps_samples = input_samples[self.batch_shuffle_idx], output_samples[self.batch_shuffle_idx], steps_samples[self.batch_shuffle_idx]
+
+    return input_samples, output_samples, steps_samples, [self.data['id']]*self.num_samples
+
+  def __len__(self):
+    '''
+    Returns the number of samples in the dataset.
+
+    Returns:
+      int: Number of samples in the dataset.
+    '''
+    return self.num_samples
+
+  def __getitem__(self, idx):
+    '''
+    Returns a sample from the dataset at the given index.
+
+    Args:
+        idx (int): Index of the sample.
+
+    Returns:
+        tuple: A tuple containing the input, output, and steps for the sample.
+    '''
+
+    return self.input_samples[idx], self.output_samples[idx], self.steps_samples[idx], self.data['id']
