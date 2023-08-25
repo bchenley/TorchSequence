7b166f9ae940275d46de98f429df96e25fa9d70a
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-25 15:38:32 -0400

    Update setup.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/setup.py
+++ ts_src/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name = 'TorchTimeSeries', 
+setup(name = 'torchts',
       version = '0.1',
       packages = find_packages(), 
       install_requires = ['torch',
