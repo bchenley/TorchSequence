872dd034e1aa0e6858216a2f1b77cc56d8bfdb4d
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-25 17:17:08 -0400

    Update __init__.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/__init__.py
+++ ts_src/__init__.py
@@ -1,46 +1,49 @@
 print("Initializing TorchTimeSeries package...")
 
-import importlib
+from setuptools import find_packages
+# import importlib
 
-__all__ = ['ExploratoryTimeSeriesAnalysis',
-           'FeatureTransform',
-           'Criterion', 
-           'fft', 
-           'periodogram', 
-           'moving_average', 
-           'butter', 
-           'fill', 
-           'Interpolator', 
-           'remove_outliers', 
-           'BaselineModel', 
-           'Polynomial', 
-           'LRU', 
-           'HiddenLayer', 
-           'ModulationLayer',
-           'LegendreModulator',
-           'ChebychevModulator', 
-           'FourierModulator', 
-           'SigmoidModulator', 
-           'Attention', 
-           'TransformerEncoderLayer',
-           'TransformerDecoderLayer', 
-           'CNN1D',
-           'SequenceModelBase', 
-           'SequenceModel', 
-           'Seq2SeqModel', 
-           'Embedding', 
-           'PositionalEncoding', 
-           'SequenceDataset',                        
-           'SequenceDataloader',
-           'TimeSeriesDataModule',
-           'SequenceModule',
-           'ARIMA',
-           'Naive',
-           'MovingAverage']
+__all__ = find_packages()
 
-for module_name in __all__:
-    module = importlib.import_module(f'.{module_name}', __name__)
-    globals()[module_name] = getattr(module, module_name)
+# __all__ = ['ExploratoryTimeSeriesAnalysis',
+#            'FeatureTransform',
+#            'Criterion', 
+#            'fft', 
+#            'periodogram', 
+#            'moving_average', 
+#            'butter', 
+#            'fill', 
+#            'Interpolator', 
+#            'remove_outliers', 
+#            'BaselineModel', 
+#            'Polynomial', 
+#            'LRU', 
+#            'HiddenLayer', 
+#            'ModulationLayer',
+#            'LegendreModulator',
+#            'ChebychevModulator', 
+#            'FourierModulator', 
+#            'SigmoidModulator', 
+#            'Attention', 
+#            'TransformerEncoderLayer',
+#            'TransformerDecoderLayer', 
+#            'CNN1D',
+#            'SequenceModelBase', 
+#            'SequenceModel', 
+#            'Seq2SeqModel', 
+#            'Embedding', 
+#            'PositionalEncoding', 
+#            'SequenceDataset',                        
+#            'SequenceDataloader',
+#            'TimeSeriesDataModule',
+#            'SequenceModule',
+#            'ARIMA',
+#            'Naive',
+#            'MovingAverage']
+
+# for module_name in __all__:
+#     module = importlib.import_module(f'.{module_name}', __name__)
+#     globals()[module_name] = getattr(module, module_name)
 
            
 print("Done")
