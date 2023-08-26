789971df73f7fb7efa1ec43a2fa0ea7788a41546
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-26 12:41:31 -0400

    Update ExploratoryTimeSeriesAnalysis.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/ExploratoryTimeSeriesAnalysis.py
+++ ts_src/ExploratoryTimeSeriesAnalysis.py
@@ -2,8 +2,8 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-import moving_average
-import fft
+from moving_average import moving_average
+from fft import fft
 
 class ExploratoryTimeSeriesAnalysis():
   """
