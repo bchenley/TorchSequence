045fa3fe05bb7dac6ea1eeef82eb24424b90d334
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-26 12:54:39 -0400

    Update SequenceDataloader.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/SequenceDataloader.py
+++ ts_src/SequenceDataloader.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-from SequenceDataset import SequenceDataset
+from ts_src.SequenceDataset import SequenceDataset
 
 class SequenceDataloader(torch.utils.data.Dataset):
 
