82cc49c7dc3c65ec9364f6d3bd0313c5613efa83
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-26 11:44:42 -0400

    Update __init__.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/__init__.py
+++ ts_src/__init__.py
@@ -2,50 +2,54 @@ print("Initializing TorchTimeSeries package...")
 
 import importlib
 import pkgutil
+# os
 
-# __all__ = [name for _,name,_ in pkgutil.iter_modules(['TorchTimeSeries.ts_src'])]
+__all__ = [name for _,name,_ in pkgutil.iter_modules(['TorchTimeSeries.ts_src'])]
 
-# print(__all__)
+files = [filename[:-3] for filename in os.listdir('TorchTimeSeries/ts_src') if filename.endswith('.py')]
 
-__all__ = ['ExploratoryTimeSeriesAnalysis',
-           'FeatureTransform',
-           'Criterion', 
-           'fft', 
-           'periodogram', 
-           'moving_average', 
-           'butter', 
-           'fill', 
-           'Interpolator', 
-           'remove_outliers', 
-           'BaselineModel', 
-           'Polynomial', 
-           'LRU', 
-           'HiddenLayer', 
-           'ModulationLayer',
-           'LegendreModulator',
-           'ChebychevModulator', 
-           'FourierModulator', 
-           'SigmoidModulator', 
-           'Attention', 
-           'TransformerEncoderLayer',
-           'TransformerDecoderLayer', 
-           'CNN1D',
-           'SequenceModelBase', 
-           'SequenceModel', 
-           'Seq2SeqModel', 
-           'Embedding', 
-           'PositionalEncoding', 
-           'SequenceDataset',                        
-           'SequenceDataloader',
-           'TimeSeriesDataModule',
-           'SequenceModule',
-           'ARIMA',
-           'Naive',
-           'MovingAverage']
+# __all__ = [module_name for module_name in files if module_name != '__init__']
+
+# __all__ = ['ExploratoryTimeSeriesAnalysis',
+#            'FeatureTransform',
+#            'Criterion', 
+#            'fft', 
+#            'periodogram', 
+#            'moving_average', 
+#            'butter', 
+#            'fill', 
+#            'Interpolator', 
+#            'remove_outliers', 
+#            'BaselineModel', 
+#            'Polynomial', 
+#            'LRU', 
+#            'HiddenLayer', 
+#            'ModulationLayer',
+#            'LegendreModulator',
+#            'ChebychevModulator', 
+#            'FourierModulator', 
+#            'SigmoidModulator', 
+#            'Attention', 
+#            'TransformerEncoderLayer',
+#            'TransformerDecoderLayer', 
+#            'CNN1D',
+#            'SequenceModelBase', 
+#            'SequenceModel', 
+#            'Seq2SeqModel', 
+#            'Embedding', 
+#            'PositionalEncoding', 
+#            'SequenceDataset',                        
+#            'SequenceDataloader',
+#            'TimeSeriesDataModule',
+#            'SequenceModule',
+#            'ARIMA',
+#            'Naive',
+#            'MovingAverage']
 
 for module_name in __all__:
     module = importlib.import_module(f'.{module_name}', __name__)
     globals()[module_name] = getattr(module, module_name)
+
 
            
 print("Done")
