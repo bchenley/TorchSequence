b6924c20728ad0576a9d36d71292afef1facc619
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-08-26 12:35:55 -0400

    Update TransformerEncoderLayer.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/TransformerEncoderLayer.py
+++ ts_src/TransformerEncoderLayer.py
@@ -1,7 +1,7 @@
 import torch 
 
-from ts_src.Attention import Attention
-from ts_src.Polynomial import Polynomial
+from Attention import Attention
+from Polynomial import Polynomial
 
 class TransformerEncoderLayer(torch.nn.TransformerEncoderLayer):
     '''
