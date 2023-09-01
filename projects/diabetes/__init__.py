734ae0f1f2e3c6c891205c9faf0051c499dbb841
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-09-01 15:32:08 -0400

    Create __init__.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

---
+++ projects/diabetes/__init__.py
@@ -0,0 +1,13 @@
+print("Initializing FGFSD Repo...")
+
+import importlib
+
+__all__ = ['collect_data',
+           'describe']
+
+for module_name in __all__:
+    module = importlib.import_module(f'.{module_name}', __name__)
+    globals()[module_name] = getattr(module, module_name)
+
+           
+print("Done")
