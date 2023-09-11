61f33ae9d458489fed3cd18c94c61a162d2fa084
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-09-11 15:53:53 -0400

    Update TimeSeriesDataModule.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/TimeSeriesDataModule.py
+++ ts_src/TimeSeriesDataModule.py
@@ -26,7 +26,7 @@ class TimeSeriesDataModule(pl.LightningDataModule):
                time_unit = 's',
                input_unit = [None], output_unit = [None],
                pad_data = False,
-               # shuffle_train = False,
+               shuffle_train = False,
                print_summary = False,
                device = 'cpu', dtype = torch.float32):
 
@@ -506,7 +506,7 @@ class TimeSeriesDataModule(pl.LightningDataModule):
 
     return self.forecast_dl.dl
 
-  def train_dataloader(self, shuffle):
+  def train_dataloader(self):
     """
     Creates and returns a dataloader for training data.
 
@@ -529,7 +529,7 @@ class TimeSeriesDataModule(pl.LightningDataModule):
             shift=self.shift,
             stride=self.stride,
             init_input=self.train_init_input,
-            shuffle=shuffle,
+            shuffle=self.shuffle_train,
             print_summary=self.print_summary,
             device=self.device,
             dtype=self.dtype
