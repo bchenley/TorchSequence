171d1a234dbb203ad3db05c82d76ec740cc9805b
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-09-27 22:03:22 -0400

    Update clear_cuda_tensors.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/clear_cuda_tensors.py
+++ ts_src/clear_cuda_tensors.py
@@ -1,6 +1,6 @@
 import torch
 
-def clear_cuda_tensors(glbs = globals()):
+def clear_cuda_tensors(glbs = globals(), lcls = locals()):
   vars_to_del = []
   for name, var in glbs.items():
     if isinstance(var, torch.Tensor):
@@ -14,3 +14,17 @@ def clear_cuda_tensors(glbs = globals()):
   for name in vars_to_del:
         del glbs[name]
         glbs[name] = None
+  
+  vars_to_del = []
+  for name, var in lcls.items():
+    if isinstance(var, torch.Tensor):
+       if var.device == 'cuda':
+         vars_to_del.append(name)
+    elif isinstance(var, list):
+      if all(isinstance(var_, torch.Tensor) for var_ in var):
+        if var.device == 'cuda':
+          vars_to_del.append(name)
+
+  for name in vars_to_del:
+        del lcls[name]
+        lcls[name] = None
