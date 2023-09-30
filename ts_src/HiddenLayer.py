d4db48f4f1648b484d5b285e5d7980ce5dd1d34b
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-09-29 23:07:02 -0400

    Update HiddenLayer.py
    Signed-off-by: Brandon Henley <47284460+bchenley@users.noreply.github.com>

--- ts_src/HiddenLayer.py
+++ ts_src/HiddenLayer.py
@@ -137,14 +137,15 @@ class HiddenLayer(torch.nn.Module):
           raise ValueError(f"Number of groups ({self.groups}) cannot be larger than the number of input features ({self.in_features}) or output features ({self.out_features}).")
 
         # Calculate the number of full groups and the size of the last group
-        group_size = self.in_features // self.groups
+        row_size = self.out_features // self.groups
+        col_size = self.in_features // self.groups
         
         # Determine group sizes
-        row_sizes = [group_size] * self.groups
-        col_sizes = [group_size] * self.groups
+        row_sizes = [row_size] * self.groups
+        col_sizes = [col_size] * self.groups
 
-        if sum(row_sizes) < self.in_features: row_sizes[-1] += self.in_features - sum(row_sizes)
-        if sum(col_sizes) < self.out_features: col_sizes[-1] += self.out_features - sum(col_sizes)
+        if sum(row_sizes) < self.out_features: row_sizes[-1] += self.out_features - sum(row_sizes)
+        if sum(col_sizes) < self.in_features: col_sizes[-1] += self.in_features - sum(col_sizes)
         
         # Generate the mask based on group sizes
         self.group_mask = torch.zeros_like(self.F[0].weight,
@@ -152,9 +153,9 @@ class HiddenLayer(torch.nn.Module):
         
         i,j = 0,0
         for row_size, col_size in zip(row_sizes, col_sizes):        
-          self.group_mask[i:(i+row_size), j:(j+col_size)] = 1.
-          i += row_size
+          self.group_mask[i:(i+row_size), j:(j+col_size)] = 1.          
           j += col_size
+          i += row_size
 
     def forward(self, input):
         """
